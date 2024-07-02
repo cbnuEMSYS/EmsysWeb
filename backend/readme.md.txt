38722b00376a98e202d241c5c9958140f9795970
Kim Minseok <minseok@chungbuk.ac.kr>
2024-07-02 21:45:46 +0900

    백엔드 프로젝트 뼈대 추가
    - Spring Initializr로 생성된 백엔드 프로젝트 뼈대 업로드
    - readme.md 참조

---
+++ backend/readme.md.txt
@@ -0,0 +1,13 @@
+## 멤버
+- 이용준(역할분담 보류)
+- 김민석(DB 설계
+- 이동현(기본기능)
+- 신동연(폴더 형식 구상)
+
+## 사용 프레임워크
+dependency 관리 : gradle(groovy)
+기본 spring 프레임워크 : Springboot
+템플릿 엔진 : thymeleaf
+DB : jpa, hibernate validator, mysql
+보안 : springsecurity
+기타 : lombok
---
+++ backend/src/main/java/com/example/emsysweb/EmsyswebApplication.java
@@ -0,0 +1,13 @@
+package com.example.emsysweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsyswebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmsyswebApplication.class, args);
+	}
+
+}
