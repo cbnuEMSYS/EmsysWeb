0832643b3886c05fd2d34563924af7728b511314
JaeChanHwang <57826563+EmpChan@users.noreply.github.com>
2024-08-02 19:53:01 +0900

    Add controller and dependency
    MainController and add dependency for auto update

--- src/main/java/com/applications/MainController.java
+++ src/main/java/com/applications/MainController.java
@@ -3,10 +3,11 @@ package com.applications;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class MainController {
 
     @GetMapping("/") // localhost:8080/
     public String main() {
-        return ""
+        return "navBar";
     }
 }
