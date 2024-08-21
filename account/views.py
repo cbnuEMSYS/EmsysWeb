19050c0291bd0a8e25c15bd2493dc26fa5753df8
KimMinseong <rjaehno2@naver.com>
2024-08-21 16:21:14 +0900

    feat: Create view of board

--- account/views.py
+++ account/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render, redirect
 
 def test(request):
     return redirect('main:mainpage')
+
+def board_list(request):
+    return render(request, 'board/board_list.html', {})
