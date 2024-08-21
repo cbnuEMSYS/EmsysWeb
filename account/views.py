ea598c7e2f4c2704c5d2ec40693e1d5e3b2b009f
KimMinseong <rjaehno2@naver.com>
2024-08-21 16:31:22 +0900

    Fix:Set views of board appropriately

--- account/views.py
+++ account/views.py
@@ -2,6 +2,3 @@ from django.shortcuts import render, redirect
 
 def test(request):
     return redirect('main:mainpage')
-
-def board_list(request):
-    return render(request, 'board/board_list.html', {})
