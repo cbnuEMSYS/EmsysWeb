e6fa4929d9939807f92db565881412355e816da4
KimMinseong <rjaehno2@naver.com>
2024-08-26 16:42:30 +0900

    boar/views파일 내 요소 이름 변경

--- board/views.py
+++ board/views.py
@@ -3,5 +3,18 @@ from django.shortcuts import render, redirect
 def test(request):
     return redirect('main:mainpage')
 
+#def board_list(request):
+#    return render(request, 'board/board_list.html')
+
 def board_list(request):
-    return render(request, 'board/board_list.html')
+    context = {
+        'title': 'Board list',
+        'board_list': [
+            {'no':1, 'title': '목록1' },
+            {'no':2, 'title': '목록2' },
+            {'no':3, 'title': '목록3' },
+            {'no':4, 'title': '목록4' },
+            {'no':5, 'title': '목록5' }
+        ]
+    }
+    return render(request, 'board/board_list.html', context)
